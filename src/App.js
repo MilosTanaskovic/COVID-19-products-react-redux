68c938c19f3aa5dbbf7947538e461238b1f3e52e
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2021-03-18 11:20:22 +0100

    create app and clear src folder...

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>covid19 products</h1>
+    </div>
+  );
+}
+
+export default App;

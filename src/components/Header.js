c770850e8038f678b60b7fe6663895ba00736aa5
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2021-03-19 10:35:51 +0100

    create components and pages for upgrade app later... install material ui...

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// material-ui
+
+/**
+* @author Milos Tanaskovic
+* @function Header
+**/
+
+const Header = (props) => {
+  return(
+    <div>Header</div>
+   )
+  }
+
+
+export default Header

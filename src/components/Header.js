8db85b2b1e3a921c8acd6b8c11efc6171ac076b2
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2021-03-21 18:07:05 +0100

    create card product....

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,11 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
 import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-
-// import API
-import Api from '../api';
 
 /**
 * @author Milos Tanaskovic
--- src/components/Products.js
+++ src/components/Products.js
@@ -3,6 +3,11 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 /**
 * @author Milos Tanaskovic
@@ -13,10 +18,23 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    height: 140,
-    width: 100,
+  card: {
+    width: 300,
   },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'space-around'
+  },
+  title: {
+    fontSize: 22,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  price: {
+    fontSize: 21,
+    color: 'primary',
+  }
 }));
 
 const Products = ({pruduct}) => {
@@ -24,7 +42,39 @@ const Products = ({pruduct}) => {
 
   return(
     <Grid key={pruduct} item>
-      <Paper className={classes.paper} />
+
+      <Card className={classes.card}>
+        <CardContent>
+          {/* title */}
+          <Typography 
+            className={classes.title} 
+            color="primary" 
+            noWrap 
+            gutterBottom
+          >
+            COVID-19 PCR afdfdsafafdadsad
+          </Typography>
+          {/* description */}
+          <Typography className={classes.pos} color="textSecondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur adipisci sapiente minima veritatis dignissimos facere soluta praesentium fuga. Provident dicta eveniet officiis velit sed veniam eum, ea ducimus vel ipsam.
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae officiis autem quis reiciendis aut aspernatur ducimus, itaque porro at, cumque suscipit rerum ipsum nobis id dolore impedit repellat repudiandae neque?
+          </Typography>
+          <Typography variant="body2" component="p" noWrap>
+            1 marcador
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.cardActions}>
+          <Typography className={classes.price} color="textSecondary">
+            152E
+          </Typography>
+          <Button size="small" variant="contained" color="primary">
+            Add to cart
+          </Button>
+        </CardActions>
+      </Card>
+
     </Grid>
    );
   }
